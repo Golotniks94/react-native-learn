b0779f1d2016bcfffba604439a03d6f3d841204d
Vladimirs <98471773+Golotniks94@users.noreply.github.com>
2023-04-06 18:35:53 +0300

    Fix №2

---
+++ myNewProject/src/App.js
@@ -0,0 +1,13 @@
+import { RegistrationScreen } from './Screens/RegistrationScreen'
+import { StatusBar } from 'expo-status-bar'
+import { LoginScreen } from './Screens/LoginScreen'
+
+export default function App() {
+  return (
+    <>
+      {/* <RegistrationScreen /> */}
+      <LoginScreen />
+      <StatusBar style="auto" />
+    </>
+  )
+}
